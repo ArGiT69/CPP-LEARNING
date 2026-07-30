efbf76b03d65412dc3d8e6b49640f3477cca376a
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 18:41:12 +0545

    Format code in type_conversion.cpp and typedef.cpp for improved readability

--- type_conversion.cpp
+++ type_conversion.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-int main(){
- int correct = 8;
- int questions = 10;
- double score = correct/(double)questions * 100;
- std::cout<< score <<"%";
+#include <iostream>
+int main()
+{
+    int correct = 8;
+    int questions = 10;
+    double score = correct / (double)questions * 100;
+    std::cout << score << "%";
     return 0;
 }
