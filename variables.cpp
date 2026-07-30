f66441a3e3052ca7df9632a3ec15b4cf65117a46
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 14:24:22 +0545

    Add initial implementation of variables.cpp with basic arithmetic and output

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    int a;//declaration
+    int p;
+    a = 7;//value assignment
+    p = 1;
+    int sum = a + p;
+    std::cout << sum;//printing the value
+
+    return 0;
+}
