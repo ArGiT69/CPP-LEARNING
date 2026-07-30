97b1ec208217f4248675bad236650d3d5cb1e2aa
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 14:20:57 +0545

    Add multi-line comment for clarity in testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -2,6 +2,11 @@
 int main(){
 
     //This is a comment
+    /*
+    This is
+    a multi line comment
+    haha!
+    */
     std::cout<<"Hello world!" <<'\n';
     // for adding a new line, <<'\n' and <<std::endl both are suitable
     std::cout<<"This is my first Program!" <<std::endl;
