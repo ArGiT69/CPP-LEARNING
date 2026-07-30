5d7bfca8b8f13888bb7a3445e7f0fcd53fd1c795
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 17:53:05 +0545

    Refactor variable declarations and comments for clarity in variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 int main()
 {
-    int a;//declaration
-    int p;
-    a = 7;//value assignment
-    p = 1;
-    int sum = a + p;
-    std::cout << sum;//printing the value
+    // //int is a variable used for integers
+    // int a;//declaration
+    // int p;
+    // a = 7;//value assignment
+    // p = 1;
+    // int sum = a + p ;
+    // std::cout << sum;//printing the value
+    //doubles is a variable used for storing point-numbers with high precision
+    double m = 6.99;
+    std::cout<< m <<'\n';
+    //Char is a variable used to store single character
+    char aalu = 'A';
+    std::cout<< aalu <<std::endl;
+    char dollarSign = '$';
+    std::cout<< dollarSign <<std::endl;
+    // boolean 
+    // boolean is only present in two states i.e. true and false
+    bool student = false;
+    bool power = true;
+    // string(objects that represents a sequence of text)
+    std::string name = "AAROHAN";
+    std::cout<< name <<std::endl;
+    std::string food = "Bhaat";
+    std::cout<< food <<std::endl;
+    std::string School = "Sukuna";
+    std::cout<< School <<std::endl;
 
     return 0;
 }
