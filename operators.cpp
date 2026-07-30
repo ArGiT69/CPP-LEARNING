db34186a96251ebe3b542f6dba238120aba44989
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 18:31:14 +0545

    Add initial implementation of operators.cpp with modulus operation demonstration

---
+++ operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+    int students = 20;
+   //students = students + 1;
+   //students = students-1;
+   //students = students * 2;
+   //students = students/4;
+   students = students%4;
+    std::cout<< students;
+    return 0;
+}
