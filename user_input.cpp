41facf676cbbbbbb9a91dffc877b96ad5c2cdedf
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-30 18:53:50 +0545

    Add initial implementation of user_input.cpp for name and age input demonstration

---
+++ user_input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+ std::string name;
+ int age;
+ std::cout << "Enter your name:\n";
+ std::cin >> name;
+ std::cout << "Enter your age:\n";
+ std::cin >> age;
+
+ std::cout << "your name is : " << name << std::endl ;
+ std::cout << "your age is : " << age << std::endl ;
+
+
+    return 0;
+}
