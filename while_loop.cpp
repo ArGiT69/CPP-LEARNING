6403f5b7c328520839f2add272b9013e86f9826f
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-05 22:49:08 +0545

    Refactor while_loop.cpp for improved formatting and readability

--- while_loop.cpp
+++ while_loop.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-int main(){
-std:: string name;
+int main()
+{
+    std::string name;
 
-while(name.empty()){
-std::cout<< "Enter your name: ";
-std::getline(std::cin, name);
-}
+    while (name.empty())
+    {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
 
-std::cout << "hello " << name;
+    std::cout << "hello " << name;
     return 0;
 }
