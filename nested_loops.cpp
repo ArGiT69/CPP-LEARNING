fba9f5e7155ab68a9ca600ba23b1aa6773c73ceb
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-05 23:26:37 +0545

    Fix nested loop syntax error and add implementation of random_event.cpp to demonstrate random number generation with switch-case statements

--- nested_loops.cpp
+++ nested_loops.cpp
@@ -14,7 +14,7 @@ int main()
 
         for (int i = 1; i <= rows; i++)
     {   
-        for (int j = 1; j <= columns;; j++)
+        for (int j = 1; j <= columns; j++)
         {
             std::cout << j << ' ';
         }
