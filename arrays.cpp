15de19cfeff496776d5cebb78ac5d97307b125e6
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-07 23:10:44 +0545

    Add implementation of arrays.cpp to demonstrate array usage and output in C++

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+    // std::string name[] = {"Alice", "Bob", "Charlie", "David", "Eve"};
+    // std::cout << name[3];
+
+
+    double prices[] = {6.99,6.21,5.16,1.79};
+
+    std::cout << prices[0] << '\n';
+    std::cout << prices[1] << '\n';
+    std::cout << prices[2] << '\n';
+    std::cout << prices[3] << '\n';
+
+
+    return 0;
+}
