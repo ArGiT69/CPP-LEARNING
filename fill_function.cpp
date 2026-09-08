5f09be472b6cb6d108f85e4981b792efdc2515f4
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-08 13:15:49 +0545

    Add implementation of fill_function.cpp to demonstrate filling an array with specified values

---
+++ fill_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    // fill() = Fills a range of elements with a specified value
+    //          fill( begin , end, value)
+    const int SIZE = 100;   
+    std::string foods[SIZE];
+
+    fill(foods, foods + (SIZE/2), "pizza");
+    fill(foods + (SIZE/2), foods + SIZE, "ice cream");
+    for (std::string food : foods)
+    {
+        std::cout << food << '\n';
+    }
+    return 0;
+}
