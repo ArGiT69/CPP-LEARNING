70b6bb17989a2e4313bb3849bcb49d9eac83b342
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-08 13:53:24 +0545

    Refactor fill_function.cpp to implement user input for food items and remove previous fill functionality

--- fill_function.cpp
+++ fill_function.cpp
@@ -3,14 +3,26 @@ int main()
 {
     // fill() = Fills a range of elements with a specified value
     //          fill( begin , end, value)
-    const int SIZE = 100;   
-    std::string foods[SIZE];
-
-    fill(foods, foods + (SIZE/2), "pizza");
-    fill(foods + (SIZE/2), foods + SIZE, "ice cream");
-    for (std::string food : foods)
+    //          we will get the user input and use the fill() function to fill the array
+    std::string food[5];
+    int size = sizeof(food) / sizeof(food[0]);
+    std::string temp;
+    for(int i = 0; i < size; i++)
     {
-        std::cout << food << '\n';
+        std::cout << "Enter food items you like or click \"q\" to quit " << i + 1  << ": ";
+        std::getline(std::cin, temp);
+        if(temp == "q" || temp == "Q")
+        {
+            break;
+        }
+        else{
+            food[i] = temp;
+        }
+    }
+ std::cout << "\nThe food items you entered are: \n";
+    for(int i = 0; !food[i].empty(); i++)
+    {
+        std::cout << food[i] << std::endl;
     }
     return 0;
 }
