c1209dc9f122b60fa0dfadeffcbfd9a9c347e037
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-08 14:05:17 +0545

    Add implementation of multidimensional_array.cpp to demonstrate usage of a 2D array for car names

---
+++ multidimensional_array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+int main(){
+
+    std::string cars[][3] = {{"Mustang", "Escape", "f-150"},
+                            {"corvette", "equinox", "silverado"},
+                            {"Challenger", "Durango", "Ram 1500"}};
+                            
+                            // std::cout << cars[0][0] << "\n ";
+                            // std::cout << cars[0][1] << "\n ";
+                            // std::cout << cars[0][2] << "\n ";
+                            // std::cout << cars[1][0] << "\n ";
+                            // std::cout << cars[1][1] << "\n ";
+                            // std::cout << cars[1][2] << "\n ";
+                            // std::cout << cars[2][0] << "\n ";
+                            // std::cout << cars[2][1] << "\n ";
+                            // std::cout << cars[2][2] << "\n ";
+                            
+                            int rows = sizeof(cars)/sizeof(cars[0]);
+                            int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+                            for(int i = 0; i < rows; i++){
+                                for(int j = 0; j < columns; j++){
+                                std::cout << cars[i][j] << " ";       
+                            }
+                            std::cout << '\n';
+
+                            }
+    return 0;   
+}   
