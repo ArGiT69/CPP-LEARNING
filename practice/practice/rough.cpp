e7438bb33c37c85b1d30cf2a8272469d35fe6874
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-08 16:50:59 +0545

    Refactor rough.cpp to implement string swapping functionality and remove sorting logic

--- practice/practice/rough.cpp
+++ practice/practice/rough.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
-void sort(int array[], int size);
+void swap(std::string &x, std::string &y);
 int main()
 {
-    int arr[] = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    sort(arr, size);
- for(int element : arr){
-    std::cout << element << ' ';
- }
+    std::string x = "Kool-Aid";
+    std::string y = "Water";
+    swap(x,y);
+    std::cout << "X: " << x << '\n';
+    std::cout << "Y: " << y << '\n';
     return 0;
 }
-void sort(int array[], int size)
+void swap(std::string &x, std::string &y)
 {
-
-    int temp;
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
+    std::string temp;
+    temp = x;
+    x = y;
+    y = temp;
 }
