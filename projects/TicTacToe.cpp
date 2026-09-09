e961ec2c5d43c4d27ae1c78df68c02471396d6ab
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-09 16:59:43 +0545

    Add initial implementation of TicTacToe game structure

---
+++ projects/TicTacToe.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computeMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+
+int main(){
+    
+    
+    
+    return 0; 
+}
+void drawBoard(char *spaces){
+
+
+}
+void playerMove(char *spaces, char player){
+
+
+
+}
+void computeMove(char *spaces, char computer){
+
+
+
+}
+bool checkWinner(char *spaces, char player, char computer){
+
+
+
+
+}
+bool checkTie(char *spaces){
+
+
+
+    
+}
