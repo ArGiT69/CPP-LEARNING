13f6cf94c20c841cc712d1abf1f88bca1394e120
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-09 16:56:37 +0545

    Format null_pointers.cpp for improved readability

--- null_pointers.cpp
+++ null_pointers.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int *pointer = nullptr; // pointer is initialized to null
     int x = 123;
     pointer = &x; // pointer now points to the address of x
 
-    if(pointer == nullptr){ // check if pointer is null
+    if (pointer == nullptr)
+    { // check if pointer is null
         std::cout << "Pointer is null" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Pointer is not null, value: " << *pointer << std::endl; // dereference pointer to get value of x
     }
 
