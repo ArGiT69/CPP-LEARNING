1554ac5a6ed6c77fcbfae2dec108cd90d012b8f1
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-09 17:05:36 +0545

    Format TicTacToe.cpp for improved readability

--- projects/TicTacToe.cpp
+++ projects/TicTacToe.cpp
@@ -8,35 +8,36 @@ bool checkWinner(char *spaces, char player, char computer);
 bool checkTie(char *spaces);
 
 
-int main(){
-    
-    
+int main(){ 
+    char space[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+        bool running = true;
+        drawBoard(space);
     
     return 0; 
 }
 void drawBoard(char *spaces){
-
-
+    std::cout << '\n';
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "  " << std::endl;
+    std::cout << "____|_____|_____" << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "  " << std::endl;
+    std::cout << "____|_____|_____" << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "  " << std::endl;
+    std::cout << '\n';
 }
 void playerMove(char *spaces, char player){
-
-
 
 }
 void computeMove(char *spaces, char computer){
 
-
-
 }
 bool checkWinner(char *spaces, char player, char computer){
-
-
-
-
+return 0;
 }
 bool checkTie(char *spaces){
-
-
-
-    
+return 0;
 }
