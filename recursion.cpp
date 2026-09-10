cead8ec37f18432776035c1dbae376d21b21cc08
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-10 20:21:45 +0545

    Refactor walk function to implement recursion instead of iteration in recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 void walk(int steps);
-// this is an example of itteration using a for loop. The function walk takes an integer parameter steps and prints a message for each step taken. The main function calls walk with 100 steps, resulting in the message being printed 100 times.
+//this is a recursive approach
 int main(){
     
     walk(100);
@@ -8,7 +8,8 @@ int main(){
     return 0;
 }
 void walk(int steps){
-for(int i = 0; i < steps; i++){
-std::cout << " You take a new step" << std::endl;
-}
+    if(steps > 0){
+        std::cout << "You take a step! \n";
+        walk(steps - 1); 
+    }
 }
