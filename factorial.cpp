046d9e9e253e854ca37ccbffe648ad9aefff0880
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-12 17:56:17 +0545

    Add iterative factorial calculation example in factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+//this prorgram shows how to calculate the factorial of a given number using iterative approach in C++
+#include<iostream>
+int factorial(int num);
+int main(){
+    
+    std::cout << factorial(10);
+    
+    
+    return 0;
+}
+int factorial(int num){
+
+    int result = 1;
+    for(int i = 1; i <= num; i++){
+        result = result * i;
+    }
+    return result;
+}
