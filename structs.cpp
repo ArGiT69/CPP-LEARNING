2bbec094585e3d7002acfac4fb81fa62f88bb752
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-12 18:13:27 +0545

    Add initial implementation of structs in structs.cpp

---
+++ structs.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+// struct = A structure that group related variables under one name 
+//          structs can contain many different data types (strings,int,double, boolean,etc)
+//          variables in a struct are known as "members"
+//          members can be accessed with . "Class members access Operators "
+    return 0;
+}
