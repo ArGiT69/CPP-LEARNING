844539c486d9904b6d090faeeb47408122a2f00b
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-19 14:30:03 +0545

    Refactor Constructors.cpp to include class definition and constructor explanation

--- Object Oriented Programming/Constructors.cpp
+++ Object Oriented Programming/Constructors.cpp
@@ -1,12 +1,31 @@
-#include<iostream>
-int main(){
- /*
- In C++, a constructor is a special member function that is automatically called
-  when an object of a class is created.
-  Its primary purpose is to initialize the object's data members and allocate resources,
-   ensuring the object starts its life in a valid state.
- */
+#include <iostream>
+using std::string;
+class Employee
+{
+public:
+    string Name;
+    string Company;
+    int Age;
+    void introduceYourself()
+    {
+        std::cout << "Name - " << Name << std::endl;
+        std::cout << "Company - " << Company << std::endl;
+        std::cout << "Age - " << Age << std::endl;
+    }
+    Employee(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+};
+int main()
+{
+    int number;
+    Employee employee1 = Employee("Parisha", "Arjit", 16);
+    employee1.introduceYourself();
 
+    Employee employee2 = Employee("Arjit", "Parisha", 16);
+    employee2.introduceYourself();
 
     return 0;
 }
