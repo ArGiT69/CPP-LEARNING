a84cf6d38208b2413f4c4505f9288a161c106850
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-19 14:12:06 +0545

    Add initial implementation of Constructors.cpp with constructor explanation

---
+++ Object Oriented Programming/Constructors.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+ /*
+ In C++, a constructor is a special member function that is automatically called
+  when an object of a class is created.
+  Its primary purpose is to initialize the object's data members and allocate resources,
+   ensuring the object starts its life in a valid state.
+ */
+
+
+    return 0;
+}
