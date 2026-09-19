3c3342c4881eca8eb6765409a450f8bb30427d26
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-19 13:31:09 +0545

    Refactor Enums.cpp to include switch statement for day enumeration

--- Enums.cpp
+++ Enums.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
-int main(){
-    // enums = a user-defined data type that consists 
-    //         of paired name-integer constants.
-    //         GREAT if you have a set of potential options
-    
+    enum Day{sunday = 0, monday = 1, tuesday = 2, wednesday = 3, thursday = 4, friday = 5, saturday = 6};
 
+int main(){
+    Day today = friday ;
+
+    switch(today){
+        case 0: std::cout <<" It is Sunday! \n";
+                        break;
+        case 1: std::cout <<" It is monday! \n";
+                        break;
+        case 2: std::cout <<" It is tuesday! \n";
+                        break;
+        case 3: std::cout <<" It is wednesday! \n";
+                        break;
+        case 4: std::cout <<" It is thursday! \n";
+                        break;
+        case 5: std::cout <<" It is friday! \n";
+                        break;
+        case 6: std::cout <<" It is saturday! \n";
+                        break;
+    }
+    
     return 0;
 }
