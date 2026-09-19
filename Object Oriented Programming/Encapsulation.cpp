5a630e02e1d964dcc346f29d30e98d56314ce3d4
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-19 22:10:44 +0545

    Add Encapsulation.cpp to demonstrate encapsulation in OOP

---
+++ Object Oriented Programming/Encapsulation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+/*
+Encapsulation in object-oriented programming (OOP) is the process of bundling data (attributes)
+ and the methods (functions) that operate on that data into a single unit, such as a class.
+*/
+
+
+    return 0;
+}
