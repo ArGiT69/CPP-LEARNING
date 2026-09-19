315cadc6f768def4d6c73cdf943531c73aac99fd
ArGiT69 <arjitchamlagain777@gmail.com>
2026-09-19 13:23:05 +0545

    Add initial implementation of Enums in Enums.cpp

---
+++ Enums.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    // enums = a user-defined data type that consists 
+    //         of paired name-integer constants.
+    //         GREAT if you have a set of potential options
+    
+
+    return 0;
+}
